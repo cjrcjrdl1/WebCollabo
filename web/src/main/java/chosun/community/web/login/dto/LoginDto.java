d47f14620c65736ed131162fcc9b4c2179bb5b8a
springjoa <cjrcjrdl1@gmail.com>
2023-04-08 09:57:13 +0900

    Login 기능 구현중

---
+++ web/src/main/java/chosun/community/web/login/dto/LoginDto.java
@@ -0,0 +1,14 @@
+package chosun.community.web.login.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+public class LoginDto {
+
+    @NotEmpty
+    private String loginId;
+
+    @NotEmpty
+    private String password;
+}
