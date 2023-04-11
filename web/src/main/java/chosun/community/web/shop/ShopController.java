a0962b1f8f240500f9008af8fb48a1759ff86e98
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-04-11 23:46:39 +0900

    수정

--- web/src/main/java/chosun/community/web/shop/ShopController.java
+++ web/src/main/java/chosun/community/web/shop/ShopController.java
@@ -4,7 +4,6 @@ import chosun.community.web.member.dto.MemberRequestDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ShopController {
